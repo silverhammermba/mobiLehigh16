234e09d11a2ae62f55fdc8ddb86e47113232d411
Max <silverhammermba@gmail.com>
2016-02-24 13:37:01 -0500

    make face nodes springy

--- Assets/Scripts/FaceController.cs
+++ Assets/Scripts/FaceController.cs
@@ -7,13 +7,30 @@ public class Edge
 {
 	public FaceNode n1;
 	public FaceNode n2;
-	public float strength = 2f;
+	public float strength = 2f; // the spring constant
+	public float length; // the spring's equilibrium length, computed from initial position
 
 	public Edge(FaceNode _n1, FaceNode _n2, float _strength = 2f)
 	{
 		n1 = _n1;
 		n2 = _n2;
 		strength = _strength;
+	}
+
+	public float CurrentLength()
+	{
+		return Vector3.Distance(n1.transform.position, n2.transform.position);
+	}
+
+	public void ApplyForce()
+	{
+		float diff = CurrentLength() - length;
+
+		// direction from n1 to n2
+		Vector2 dir = (n2.transform.position - n1.transform.position).normalized;
+
+		n1.Body.AddForce(strength * diff * dir);
+		n2.Body.AddForce(strength * diff * -dir);
 	}
 }
 
@@ -25,7 +42,8 @@ public class FaceController : MonoBehaviour
 	void Start ()
 	{
 		// mirror all (non-center) nodes
-		for (int i = 0; i < transform.childCount; ++i)
+		int numNodes = transform.childCount;
+		for (int i = 0; i < numNodes; ++i)
 		{
 			FaceNode n1 = transform.GetChild(i).GetComponent<FaceNode>();
 
@@ -37,7 +55,7 @@ public class FaceController : MonoBehaviour
 
 			GameObject newNode = Instantiate(node, new Vector3(-n1.transform.position.x, n1.transform.position.y, n1.transform.position.z), Quaternion.identity) as GameObject;
 			newNode.name = n1.gameObject.name + " (mirror)";
-			//newNode.transform.SetParent(transform);
+			newNode.transform.SetParent(transform);
 
 			FaceNode n2 = newNode.GetComponent<FaceNode>();
 			n1.mirror = n2;
@@ -48,7 +66,29 @@ public class FaceController : MonoBehaviour
 		int numEdges = edges.Count;
 		for (int i = 0; i < numEdges; ++i)
 		{
+			// if it's between two central nodes, don't duplicate it
+			if (edges[i].n1.mirror == edges[i].n1 && edges[i].n2.mirror == edges[i].n2.mirror) continue;
+
 			edges.Add(new Edge(edges[i].n1.mirror, edges[i].n2.mirror, edges[i].strength));
+		}
+
+		// compute edges lengths
+		for (int i = 0; i < edges.Count; ++i)
+		{
+			edges[i].length = edges[i].CurrentLength();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		for (int i = 0; i < edges.Count; ++i)
+		{
+			edges[i].ApplyForce();
+		}
+
+		for (int i = 0; i < transform.childCount; ++i)
+		{
+			transform.GetChild(i).GetComponent<FaceNode>().ApplyForce();
 		}
 	}
 }
--- Assets/Scripts/FaceNode.cs
+++ Assets/Scripts/FaceNode.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class FaceNode : MonoBehaviour
 {
-	int side;
-	Vector3 start;
 	public FaceNode mirror = null;
+
+	int side;
+	Rigidbody2D rb;
+	Vector3 start; // starting position
+	const float strength = 3f; // the spring constant for this node returning to start
 
 	void Awake()
 	{
 		side = transform.position.x == 0f ? 0 : (int)Mathf.Sign(transform.position.x);
 		start = transform.position;
+		rb = GetComponent<Rigidbody2D>();
 
 		if (transform.position.z != 0f)
 			Debug.Log("WARNING: Node starting at wrong Z plane!", gameObject);
@@ -19,5 +23,20 @@ public class FaceNode : MonoBehaviour
 	public int Side
 	{
 		get { return side; }
+	}
+
+	public Rigidbody2D Body
+	{
+		get { return rb; }
+	}
+
+	public void ApplyForce()
+	{
+		float diff = Vector3.Distance(transform.position, start);
+
+		// direction from current position to start
+		Vector2 dir = (start - transform.position).normalized;
+
+		rb.AddForce(strength * diff * dir);
 	}
 }
